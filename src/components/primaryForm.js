d34144f5ef187ad104eee3bfc25a7e589ff62929
UgurMamak <ugurmamak98@gmail.com>
2021-12-21 15:05:56 +0300

    updated

---
+++ src/components/primaryForm.js
@@ -0,0 +1,252 @@
+import React, {Component} from 'react';
+import {Button, Col, Form, Row, Container} from 'react-bootstrap';
+import axios from "axios";
+import {validate} from "../helpers/validate";
+
+class PrimaryForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            form2: {
+                email: {
+                    value: null,
+                    rules: {
+                        required: true,
+                        email: true
+                    },
+                    messages: {
+                        required: 'email boş bırakılamaz',
+                        email: 'geçersiz email adresi'
+                    }
+                },
+                email2:{
+                    rules:{
+                        email:true,
+                        required:true
+                    }
+                },
+                password: {
+                    value: null,
+                    rules: {
+                        required: true,
+                        maxLength:8,
+                        minLength:2
+                    },
+                    messages: {
+                        required: 'password boş bırakılamaz'
+                    }
+                },
+                password2:{
+                    rules:{
+                        equalTo: 'password'
+                    }
+                },
+                citySelect: {
+                    value: '',
+                    rules: {
+                        required: true
+                    },
+                    messages: {
+                        required: 'şehir boş bırakılamaz'
+                    }
+                },
+                checkbox2: {
+                    value: null,
+                    rules: {
+                        required: true
+                    },
+                    messages: {
+                        required: "checkbox2 boş bırakılamaz"
+                    }
+                },
+                checkbox1: {
+                    value: null,
+                    rules: {
+                        required: true
+                    }
+                },
+                count:{
+                    rules:{
+                        isNumber:true
+                    }
+                },
+                formHorizontalRadios:{
+                    rules:{
+                        required:true
+                    }
+                }
+            },
+            errors: {}
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        var name = event.target.name,
+            value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+
+        this.setState(prevState => ({
+            form2: {
+                ...prevState.form2,
+                [name]: {
+                    ...prevState.form2[name],
+                    value: value
+                }
+            }
+        }));
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var data = {};
+
+        const {formIsValid, errors} = validate(this.state.form2);
+
+        this.setState({
+            errors
+        });
+
+        if (formIsValid) {
+            Object.keys(this.state.form2).forEach(formItem => {
+                data[formItem] = this.state.form2[formItem]["value"]
+            });
+
+            axios.post("https://jsonplaceholder.typicode.com/posts/1/comments", data).then(response => {
+                console.log(response)
+            });
+        }
+
+    }
+
+    render() {
+        return (
+            <Container>
+                <Form onSubmit={this.handleSubmit}>
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="text" className={'required'} data-msg-required="boş bırakmayınız."
+                                          onChange={(e) => this.handleChange(e)}
+                                          name="email" placeholder="Enter email"/>
+                            <span style={{color: "red"}}>{this.state.errors["email"]}</span>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridEmail2">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="text" className={'required'} data-msg-required="boş bırakmayınız."
+                                          onChange={(e) => this.handleChange(e)}
+                                          name="email2" placeholder="Enter email"/>
+                            <span style={{color: "red"}}>{this.state.errors["email2"]}</span>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="password" onChange={(e) => this.handleChange(e)}
+                                          placeholder="Password"/>
+                            <span style={{color: "red"}}>{this.state.errors["password"]}</span>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridPassword2">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="password2" onChange={(e) => this.handleChange(e)}
+                                          placeholder="Password2"/>
+                            <span style={{color: "red"}}>{this.state.errors["password2"]}</span>
+                        </Form.Group>
+
+                    </Row>
+
+                    <Form.Group className="mb-3" controlId="formGridAddress1">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control placeholder="1234 Main St"/>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor"/>
+                    </Form.Group>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control/>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Select defaultValue="Choose..." name="citySelect"
+                                         onChange={(e) => this.handleChange(e)}>
+                                <option>Choose...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </Form.Select>
+                            <span style={{color: "red"}}>{this.state.errors["citySelect"]}</span>
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control/>
+                        </Form.Group>
+                    </Row>
+
+                    <Form.Group className="mb-3" controlId="count">
+                        <Form.Label>Adet</Form.Label>
+                        <Form.Control name="count" onChange={(e)=>this.handleChange(e)} placeholder="Adet giriniz."/>
+                        <span style={{color: "red"}}>{this.state.errors["count"]}</span>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" id="formGridCheckbox1">
+                        <Form.Check type="checkbox" name="checkbox1" onChange={(e) => this.handleChange(e)}
+                                    label="checkbox1"/>
+                        <span style={{color: "red"}}>{this.state.errors["checkbox1"]}</span>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" id="formGridCheckbox2">
+                        <Form.Check type="checkbox" name="checkbox2" onChange={(e) => {
+                            this.handleChange(e)
+                        }} label="checkbox2"/>
+                        <span style={{color: "red"}}>{this.state.errors["checkbox2"]}</span>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3">
+                        <Form.Label as="legend" column sm={2}>
+                            Radios
+                        </Form.Label>
+                        <Col sm={10}>
+                            <Form.Check
+                                type="radio"
+                                label="first radio"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios1"
+                                onChange={(e)=>this.handleChange(e)}
+
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="second radio"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios2"
+                                onChange={(e)=>this.handleChange(e)}
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="third radio"
+                                name="formHorizontalRadios"
+                                id="formHorizontalRadios3"
+                                onChange={(e)=>this.handleChange(e)}
+                            />
+                        </Col>
+                        <span style={{color: "red"}}>{this.state.errors["formHorizontalRadios"]}</span>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        );
+    }
+}
+
+export default PrimaryForm;
--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -2,16 +2,19 @@ let errors = {};
 
 const defaultMessages = {
     required: "This field is required.",
-    remote: "Please fix this field.",
+    isNumber: "Please enter a valid number.",
     email: "Please enter a valid email address.",
+    equalTo: "Please enter the same value again.",
+    maxLength: "Please enter a value less than or equal to {0}.",
+
+    remote: "Please fix this field.",
     url: "Please enter a valid URL.",
     date: "Please enter a valid date.",
     dateISO: "Please enter a valid date (ISO).",
-    number: "Please enter a valid number.",
     digits: "Please enter only digits.",
     creditcard: "Please enter a valid credit card number.",
-    equalTo: "Please enter the same value again.",
     accept: "Please enter a value with a valid extension.",
+
     /*    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
         minlength: jQuery.validator.format("Please enter at least {0} characters."),
         rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
@@ -20,10 +23,34 @@ const defaultMessages = {
         min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")*/
 };
 
+
+String.prototype.formatUnicorn = String.prototype.formatUnicorn ||
+    function () {
+        "use strict";
+        var str = this.toString();
+        if (arguments.length) {
+            var t = typeof arguments[0];
+            var key;
+            var args = ("string" === t || "number" === t) ?
+                Array.prototype.slice.call(arguments)
+                : arguments[0];
+
+            for (key in args) {
+                str = str.replace(new RegExp("\\{" + key + "\\}", "gi"), args[key]);
+            }
+        }
+
+        return str;
+    };
+
 const sendMessage = (rule, elemObject, formElem) => {
     if (elemObject.messages) {
         if (elemObject.messages[rule] === null || elemObject.messages[rule] === undefined || elemObject.messages[rule] === '') {
-            errors[formElem] = defaultMessages[rule];
+            if (rule === "maxLength") {
+                errors[formElem] = defaultMessages[rule].formatUnicorn(elemObject.rules[rule]);
+            } else {
+                errors[formElem] = defaultMessages[rule];
+            }
         } else {
             errors[formElem] = elemObject.messages[rule];
         }
@@ -41,11 +68,11 @@ const validateEmail = (value) => {
 };
 
 const validateRequired = (value) => {
-    return value === null || value === undefined || value === "" ? false : true;
+    return value === null || value === false || value === undefined || value === "" ? false : true;
 }
 
 const maxLength = (value, length) => {
-    return;
+    return value.length > length ? false : true;
 }
 
 const minLength = (value, length) => {
@@ -53,7 +80,11 @@ const minLength = (value, length) => {
 }
 
 const equalTo = (value1, value2) => {
-    return;
+    return value1 === value2 ? true : false;
+}
+
+const isNumber = (value) => {
+    return isNaN(value) ? false : true;
 }
 
 export const validate = (form) => {
@@ -65,6 +96,7 @@ export const validate = (form) => {
             elemObjectValue = elemObject.value;
 
         if (elemObject.rules) {
+
             if (elemObject.rules["email"] === true) {
                 if (!validateEmail(elemObjectValue)) {
                     let rule = "email";
@@ -73,15 +105,43 @@ export const validate = (form) => {
                 }
             }
 
+            if (elemObject.rules["isNumber"] === true) {
+                if (!isNumber(elemObjectValue)) {
+                    let rule = "isNumber";
+                    sendMessage(rule, elemObject, formElem);
+                    formIsValid = false;
+                }
+            }
+
+            if (elemObject.rules["equalTo"]) {
+                if (!equalTo(elemObjectValue, form[elemObject.rules["equalTo"]].value)) {
+                    let rule = "equalTo";
+                    sendMessage(rule, elemObject, formElem);
+                    formIsValid = false;
+                }
+            }
+
+            if (elemObject.rules["maxLength"] && elemObjectValue !== null) {
+                if (!maxLength(elemObjectValue, elemObject.rules.maxLength)) {
+                    let rule = "maxLength";
+                    sendMessage(rule, elemObject, formElem);
+                    formIsValid = false;
+                }
+            }
+
             if (elemObject.rules["required"] === true) {
+                console.log(formElem);
                 if (!validateRequired(elemObjectValue)) {
                     let rule = "required";
                     sendMessage(rule, elemObject, formElem);
                     formIsValid = false;
                 }
             }
+
         }
+
     });
+
     return {
         formIsValid, errors
     }
