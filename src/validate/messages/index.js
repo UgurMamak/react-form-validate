9a374ab4a11b3ddcfc87fa2e68230b321d76ae60
UgurMamak <ugurmamak98@gmail.com>
2021-12-27 14:59:18 +0300

    language variation added

---
+++ src/validate/messages/index.js
@@ -0,0 +1,16 @@
+import {messagesEn} from "./messages-en";
+import {messagesTr} from "./messages-tr";
+
+
+export const messages = (code) => {
+    switch (code) {
+        case 'en':
+          return   messagesEn;
+            break;
+        case 'tr':
+           return  messagesTr
+            break;
+        default:
+          return   messagesEn;
+    }
+}
